d1ce7ba3c4628db25124ad69f4632b52cf462561
Dragoneer <159856819+PurpleDragony@users.noreply.github.com>
2024-02-14 16:33:10 +0000

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,4 +1,11 @@
 import subprocess
 
+
+# man its 2:48 am and I need to be up at like 7 but I NEED TO FINISH THIS DAMN SCRIPT
+
+# ITS FUCKING 3;32 AM UGH
+# actually done with git
+
 # calls the proxy.sh setup script
-subprocess.call(["sh"], [". ./proxy.sh"])
+subprocess.run(["."], ["./proxy.sh"])
+
